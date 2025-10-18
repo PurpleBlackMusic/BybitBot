1bed37e046c96cdb5e66ac942128565932e55d3a
PurpleBlackMusic <purpleblackmusic@mail.ru>
2025-10-18 19:43:00 +0400

    Leverage runner overrides in bot helpers

---
+++ bybit_app/utils/cli/__init__.py
@@ -0,0 +1,7 @@
+"""Command-line helpers for running Bybit Spot Guardian components."""
+
+from __future__ import annotations
+
+from .bot_runner import BotCLI, BotConfig
+
+__all__ = ["BotCLI", "BotConfig"]
